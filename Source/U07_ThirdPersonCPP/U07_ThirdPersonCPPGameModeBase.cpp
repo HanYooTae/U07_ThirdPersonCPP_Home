4856287f2c512135fcc74bb06cad67ab2832348d
HanYooTae <han931120@gmail.com>
2023-08-22 14:52:07 +0900

    0822

--- Source/U07_ThirdPersonCPP/U07_ThirdPersonCPPGameModeBase.cpp
+++ Source/U07_ThirdPersonCPP/U07_ThirdPersonCPPGameModeBase.cpp
@@ -1,5 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
 #include "U07_ThirdPersonCPPGameModeBase.h"
+#include "Global.h"
 
+AU07_ThirdPersonCPPGameModeBase::AU07_ThirdPersonCPPGameModeBase()
+{
+	CHelpers::GetClass(&DefaultPawnClass, "Blueprint'/Game/Player/BP_CPlayer.BP_CPlayer_C'");
+}
