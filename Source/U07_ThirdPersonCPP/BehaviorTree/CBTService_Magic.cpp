c235e7335e6d4f3dcfdfabb6c537aea73fdd08e1
HanYooTae <han931120@gmail.com>
2023-09-27 12:41:47 +0900

    0927

---
+++ Source/U07_ThirdPersonCPP/BehaviorTree/CBTService_Magic.cpp
@@ -0,0 +1,66 @@
+#include "CBTService_Magic.h"
+#include "Components/CBehaviorComponent.h"
+#include "Components/CStateComponent.h"
+#include "Characters/CAIController.h"
+#include "Characters/CEnemy_AI.h"
+#include "Characters/CPlayer.h"
+#include "Global.h"
+
+UCBTService_Magic::UCBTService_Magic()
+{
+	NodeName = "Magic";
+}
+
+void UCBTService_Magic::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	ACAIController* controller = Cast<ACAIController>(OwnerComp.GetOwner());
+	CheckNull(controller);
+
+	UCBehaviorComponent* behaviorComp = CHelpers::GetComponent<UCBehaviorComponent>(controller);
+	CheckNull(behaviorComp);
+
+	ACEnemy_AI* enemy = Cast<ACEnemy_AI>(controller->GetPawn());
+	CheckNull(enemy);
+
+	UCStateComponent* stateComp = CHelpers::GetComponent<UCStateComponent>(enemy);
+	CheckNull(stateComp);
+
+	if (stateComp->IsHittedMode())
+	{
+		behaviorComp->SetHittedMode();
+		return;
+	}
+
+	// Get Player from BB
+	ACPlayer* player = behaviorComp->GetPlayerKey();
+
+	// No Perceived Player
+	if (player == nullptr)
+	{
+		behaviorComp->SetWaitMode();
+		controller->ClearFocus(EAIFocusPriority::Gameplay);
+		return;
+	}
+
+	// Perceived Player
+	float distance = enemy->GetDistanceTo(player);
+	
+	// yawÈ¸Àü¸¸ ½ÃÅ´
+	controller->SetFocus(player);
+
+	//-> Is in Warp(Avoid) Range (400)
+	if (distance < controller->GetBehaviorRange())
+	{
+		behaviorComp->SetAvoidMode();
+		return;
+	}
+
+	//-> Is in Action Range (1000)
+	if (distance < controller->GetSightRadius())
+	{
+		behaviorComp->SetActionMode();
+		return;
+	}
+}
